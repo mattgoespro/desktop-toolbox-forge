1d02181d42f3a91ab0f6becdc06117d32d9b1e51
Matt Young <31349794+mattgoespro@users.noreply.github.com>
2025-05-28 10:58:22 +0200

    first commit

---
+++ webpack.plugins.ts
@@ -0,0 +1,8 @@
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+
+export const plugins = [
+  new ForkTsCheckerWebpackPlugin({
+    logger: "webpack-infrastructure",
+    async: true
+  })
+];
